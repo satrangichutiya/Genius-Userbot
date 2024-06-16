f19c25cb3dc06ce19baa47084d461bea16e9332b
devgramop <168399896+devgramop@users.noreply.github.com>
2024-06-16 09:45:30 +0530

    Update help.py

--- AdityaHalder/plugins/tools/help.py
+++ AdityaHalder/plugins/tools/help.py
@@ -62,7 +62,7 @@ Genius Userbot » {__version__} ✨...
 Click On Below 🌺 Buttons To
 Get Userbot Commands.
 
-🌷Powered By : [Aditya Server](https://t.me/AdityaServer).**
+🌷Powered By : [DRAGON SERVER](https://t.me/+RLYebBEwWyYyMmQ1).**
 """
     
     if plug_match:
