1bf9a7a4fa14fb36051aa68eafff417348ed4fdc
satrangichutiya <gpyrogramxspam@gmail.com>
2025-06-14 22:40:28 +0530

    Update console.py

--- AdityaHalder/console.py
+++ AdityaHalder/console.py
@@ -6,7 +6,6 @@ from os import getenv
 from dotenv import load_dotenv
 from logging.handlers import RotatingFileHandler
 
-
 logging.basicConfig(
     format="[%(asctime)s]:[%(levelname)s]:[%(name)s]:: %(message)s",
     level=logging.INFO,
@@ -23,10 +22,8 @@ logging.getLogger("httpx").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
 
-
 if os.path.exists("Internal"):
-   load_dotenv("Internal")
-
+    load_dotenv("Internal")
 
 API_ID = int(getenv("API_ID", 0))
 API_HASH = getenv("API_HASH", None)
@@ -35,21 +32,16 @@ STRING_SESSION = getenv("STRING_SESSION", None)
 MONGO_DB_URL = getenv("MONGO_DB_URL", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", 0))
 
-
 # OPTIONAL VARIABLES
 SESSION_STRING = getenv("SESSION_STRING", None)
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", ". !").split())
 
-
-
-# OTHERS VARIABLES
-
 # PM GUARD VARS
 PM_GUARD = bool(getenv("PM_GUARD", True))
-PM_GUARD_TEXT = getenv("PM_GUARD_TEXT = getenv("PM_GUARD_TEXT", """🥀 ⡏⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿
+PM_GUARD_TEXT = getenv(
+    "PM_GUARD_TEXT",
+    """🥀 ⡏⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿
 ⡇⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⢸
-... (rest of art if any)
-""")
 ⣿⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠉⠁⠀⣿
 ⣿⣧⡀⠀⠀⠀⠀⠙⠿⠿⠿⠻⠿⠿⠟⠿⠛⠉⠀⠀⠀⠀⠀⣸⣿
 ⣿⣿⣷⣄⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿
@@ -60,18 +52,19 @@ PM_GUARD_TEXT = getenv("PM_GUARD_TEXT = getenv("PM_GUARD_TEXT", """🥀 ⡏⠉�
 ⣿⣿⣿⡀⠉⠀⠀⠀⠀⠀⢄⠀⢀⠀⠀⠀⠀⠉⠉⠁⠀⠀⣿⣿⣿
 ⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿
 ⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿
-𝐇𝐄𝐘 𝐓𝐇𝐈𝐒 𝐈𝐒 𝐇𝐈𝐆𝐇 𝐐𝐔𝐀𝐋𝐈𝐓𝐘 𝐒𝐀𝐓𝐇𝐘𝐀 𝐔𝐒𝐄𝐑𝐁𝐎𝐓 𝐌𝐘 𝐎𝐖𝐍𝐄𝐑 𝐈𝐒 𝐎𝐅𝐅𝐋𝐈𝐍𝐄 𝐏𝐋𝐄𝐀𝐒𝐄 𝐃𝐎𝐍𝐓 𝐒𝐏𝐀𝐌 𝐇𝐄𝐑𝐄 𝐁𝐄𝐀𝐂𝐔𝐒𝐄 𝐒𝐏𝐀𝐌𝐌𝐈𝐍𝐆 𝐈𝐒 𝐅𝐎𝐑𝐂𝐄 𝐌𝐄 𝐁𝐋𝐎𝐂𝐊 𝐘𝐎𝐔 𝐀𝐆𝐀𝐑 𝐒𝐏𝐀𝐌 𝐊𝐀𝐑𝐀 𝐓𝐎 𝐓𝐔 𝐑𝐀𝐍𝐖𝐀☠️.**")
+
+𝐇𝐄𝐘 𝐓𝐇𝐈𝐒 𝐈𝐒 𝐇𝐈𝐆𝐇 𝐐𝐔𝐀𝐋𝐈𝐓𝐘 𝐒𝐀𝐓𝐇𝐘𝐀 𝐔𝐒𝐄𝐑𝐁𝐎𝐓
+𝐌𝐘 𝐎𝐖𝐍𝐄𝐑 𝐈𝐒 𝐎𝐅𝐅𝐋𝐈𝐍𝐄
+𝐏𝐋𝐄𝐀𝐒𝐄 𝐃𝐎𝐍𝐓 𝐒𝐏𝐀𝐌 𝐇𝐄𝐑𝐄
+𝐁𝐄𝐂𝐀𝐔𝐒𝐄 𝐒𝐏𝐀𝐌𝐌𝐈𝐍𝐆 𝐈𝐒 𝐅𝐎𝐑𝐂𝐄 𝐌𝐄 𝐁𝐋𝐎𝐂𝐊 𝐘𝐎𝐔
+𝐀𝐆𝐀𝐑 𝐒𝐏𝐀𝐌 𝐊𝐀𝐑𝐀 𝐓𝐎 𝐓𝐔 𝐑𝐀𝐍𝐖𝐀 ☠️"""
+)
 PM_GUARD_LIMIT = int(getenv("PM_GUARD_LIMIT", 5))
-
-
 
 # USERBOT DEFAULT IMAGE
 USERBOT_PICTURE = getenv("USERBOT_PICTURE", "https://telegra.ph/file/0e3590d7b5ff939a3db57.jpg")
 
-
-
 # Don't Edit This Codes From This Line
-
 LOGGER = logging.getLogger("Genius")
 runtime = time.time()
 
@@ -81,9 +74,7 @@ PM_LIMIT = {}
 PLUGINS = {}
 SUDOERS = []
 
-
 COMMAND_HANDLERS = []
 for x in COMMAND_PREFIXES:
     COMMAND_HANDLERS.append(x)
 COMMAND_HANDLERS.append('')
-
